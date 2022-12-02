3b26466616f12f29813375f632afdfbea8e96fa8
alammar40 <119395191+alammar40@users.noreply.github.com>
2022-12-02 20:17:26 +0300

    Created the skeleton for the Program
    in this commit class where all the methods will be created is created and along with the initial logic for the program

--- Java_project_lockers/src/Main.java
+++ Java_project_lockers/src/Main.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //ArrayList<String> results = new ArrayList<String>();
+        System.out.println("Hello, and Welcome to Lockers pvt. My name is Abdulrahman Alamar,");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        char user_option = '0';
+        FileNames fileNames = new FileNames();
+        //File[] files = new File("/Users/abdulrahmanalamar/Desktop/Files_Project1").listFiles(); //If this pathname does not denote a directory, then listFiles() returns null.
+        File[] files = new File("./Files_Project1").listFiles();
+        for (File file : files) { // advanced for loop to read the file names and store them in an array list
+            if (file.isFile() && !file.isHidden()) {
+                fileNames.getResults().add(file.getName());
+            }
+        }
     }
 }
+
+class FileNames{
+    private ArrayList<String> results;
+    public FileNames() {
+        this.results = new ArrayList<>();
+    }
+    public ArrayList<String> getResults() {
+        return results;
+    }
+    public void setResults(ArrayList<String> results) {
+        this.results = results;
+    }
+
+}
+
